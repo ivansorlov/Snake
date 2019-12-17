643808d0d68c4070dd6066072cc62bb0974dcdb6
Ivan Orlov <orlov.ivans@yandex.ru>
2019-12-18 00:25:00 +0300

    Конструкторы. Инкапсуляция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -23,16 +23,16 @@ namespace snake
             //Drow(x2, y2, sym2);
 
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
+            // p1.x = 1;
+            // p1.y = 3;
+            // p1.sym = '*';
             p1.Drow();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');
+            // p2.x = 4;
+            // p2.y = 5;
+            // p2.sym = '#';
             p2.Drow();
 
             Console.ReadLine();
