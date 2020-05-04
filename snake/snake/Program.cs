a0458c3244b27e40adb1657cb69d773703e9a43b
Ivan Orlov <orlov.ivans@yandex.ru>
2020-05-05 02:20:59 +0300

    Рисуем прямые линии

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,57 +10,18 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            //int x1 = 1;
-            //int y1 = 3;
-            //char sym1 = '*';
-
-            //Drow(x1, y1, sym1);
-
-            //int x2 = 4;
-            //int y2 = 5;
-            //char sym2 = '#';
-
-            //Drow(x2, y2, sym2);
-
 
             Point p1 = new Point(1, 3, '*');
-            // p1.x = 1;
-            // p1.y = 3;
-            // p1.sym = '*';
             //p1.Drow();
 
             Point p2 = new Point(4, 5, '#');
-            // p2.x = 4;
-            // p2.y = 5;
-            // p2.sym = '#';
             //p2.Drow();
 
-            List<int> numList = new List<int>();
+            HorizontalLine line = new HorizontalLine(5, 23, 3, '.');
+            line.Drow();
 
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            // Список с нашими координатами
-
-            List<Point> pList = new List<Point>();
-
-            pList.Add(p1);
-            pList.Add(p2);
-
-            //foreach(Point i in pList)
-            //{
-                //i.Drow();
-            //}
-
-            pList[0].Drow();
+            VerticalLine vline = new VerticalLine(5, 3, 23, '.');
+            vline.Drow();
 
             Console.ReadLine();
         }
